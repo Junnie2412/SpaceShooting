7210e00fb1d678a18b5146e36651cd6a498ae17a
Junnie2412 <dungchan321@gmail.com>
2024-06-02 00:24:21 +0700

    GameComplete

---
+++ Assets/Scripts/ParalaxController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c53663f46530c0ea25ac45231c49895fd7377c4e55c6c2a960fa6260ad33d953
+size 478
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ecb8cc59a5c8f77afc0994a1a0c6fd958c9d8329b8fbf37bd49e6da2a4415584
+size 434
