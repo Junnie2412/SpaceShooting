c77c2e67a7dc78a160109da1d556eec75b90eb1e
Junnie2412 <dungchan321@gmail.com>
2024-06-01 20:05:47 +0700

    LevelAndScore

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:889d657b0130a11f98d6c792f4c4b43dd3b4be955c98595c36fa93318b9987a6
-size 612
+oid sha256:194abec79aec412c03e54da865981d8bb429d4b9ebbe5cd35aae27f79d90a20f
+size 652
--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1a9d34d205adf4aae4471ea123cc199a9af5a141f1fed5f2bcced7e2877fc63
-size 927
+oid sha256:23db144ebaefae298ff79e2a2afdefa8e5671c1c8e4b23f1ff5563eaf5322494
+size 1202
