cb7dedf2f614d8fc9db0360fffa09b0ea3742655
Junnie2412 <dungchan321@gmail.com>
2024-06-19 22:25:18 +0700

    FinalFantasy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8140327dc30324e50925fc1b5a05806f57f8fd4e0cde1a6693af7701a0d2dbf8
-size 4907
+oid sha256:e56d627a65893661031cc4aed981a628eb0eb967cf85d21f07a34d44ce5a786e
+size 4942
